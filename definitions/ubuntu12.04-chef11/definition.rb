bbe3520a1f8d7229c4441ff44e42dec25091a88f
Nikita Borzykh <sample.n@gmail.com>
2013-12-06 01:53:58 +0400

    [VBox] Use original 12.04 image and do apt-get upgrade

--- definitions/ubuntu12.04-chef11/definition.rb
+++ definitions/ubuntu12.04-chef11/definition.rb
@@ -5,9 +5,9 @@ Veewee::Session.declare({
   :disk_format => 'VDI',
   :hostiocache => 'on',
   :os_type_id => 'Ubuntu_64',
-  :iso_file => "ubuntu-12.04.3-server-amd64.iso",
-  :iso_src => "http://releases.ubuntu.com/12.04/ubuntu-12.04.3-server-amd64.iso",
-  :iso_md5 => 'af5f788aee1b32c4b2634734309cc9e9',
+  :iso_file => "ubuntu-12.04-server-amd64.iso",
+  :iso_src => "http://old-releases.ubuntu.com/releases/12.04.0/ubuntu-12.04-server-amd64.iso",
+  :iso_md5 => 'f2e921788d35bbdf0336d05d228136eb',
   :iso_download_timeout => "1000",
   :boot_wait => "4",
   :boot_cmd_sequence => [
